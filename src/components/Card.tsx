f1c94249b3ced87a27ba68cb3257b3c9ac0990bc
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-02-02 18:08:18 +0300

    Delete src/components/Card.tsx
    dele comp

--- src/components/Card.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default class Card extends React.Component<{homeLink:Link}> {
-    constructor(props) {
-        super(props);
-      }
-      
-  render() {
-    return <p>{this.props.homeLink}</p>};
-};
