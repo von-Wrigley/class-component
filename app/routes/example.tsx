4a8e9081a8e0632986e3861f1c30654d4fad3cdb
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-02-09 16:23:40 +0300

    Add files via upload

---
+++ app/routes/example.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+
+function example() {
+  return (
+    <div>
+      ccccccccccc
+      <Outlet />
+    </div>
+  )
+}
+
+export default example
