e552af5281430c378bf98e57425537c501bf4629
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-02-23 17:08:09 +0300

    Update Card.tsx

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
-import { Link, useSearchParams } from 'react-router'
+import  { useContext, useState } from 'react'
+
 import DetailedCard from './DetailedCard';
+import { useGetPeopleQuery } from '../api/StarWarsAPI';
+import Spinner from './Spinner';
+import { selected, removeItemSelected} from '../StoreSelected';
+import { useAppDispatch } from '../hooks';
+import { Context } from '../context';
+
+
+type CardType = {
+  info: DeatilesPerson,
+  key: string | undefined,
+}
+
 
 interface DeatilesPerson {
   birth_year?: string;
@@ -21,36 +33,61 @@ interface DeatilesPerson {
   vehicles?: string[];
 }
 
-function Card({data, setButton, useOpen}:any) {
-  let [searchParams, setSearchParams] = useSearchParams();
-  const value = localStorage.getItem("name")
+function Card({info}: CardType) {
+  const dispatch = useAppDispatch()
+
+
+
+  const { isLoading} = useGetPeopleQuery(1);
+
+const [useOpen, setUseOpen] = useState<boolean>(false)
+const [checked, setChecked] = useState<boolean>(false);
+const theme= useContext<string>(Context);
+
+
 
 
 
 const handleClick = ()=> {
-  setButton((prev:boolean)=> !prev)
-  const params = new URLSearchParams();
-  params.set("name", value as string);
-  setSearchParams(params, {
-    preventScrollReset: true,
-  });
-   
+
+  setUseOpen((prev:boolean)=> !prev)
+  
+
+
+
 }
 
+const handleSelected = (e: React.ChangeEvent<HTMLInputElement>)=>{ 
+  if(checked===false){
+    setChecked((prev)=> !prev)
+    dispatch(selected(e.target.value))
+  }
+  else{
+    setChecked((prev)=> !prev)
+    dispatch(removeItemSelected(e.target.value))
+  }
+}
 
-
-
+if (isLoading) return <Spinner />
   return (
-    <div>
-   {data?.filter((x:DeatilesPerson)=>x.name === value).map((d:DeatilesPerson, index:number)=>(
-          <div key={index} className='border rounded flex flex-col gap-5 bg-black text-amber-100'>
-            <p>{d.name}</p>
-            <button   onClick={handleClick} className='border p-2 hover:bg-pink-200'>подробнее</button> 
-           {useOpen===true && <DetailedCard />}
-         </div>)
-        )}
-    </div>
-  )
+ 
+
+          <div  className={theme === 'light' ? 'border rounded flex flex-row gap-5 mb-6 justify-between text-amber-100  mx-auto' : 'flex flex-row gap-5 mb-6 mx-auto text-black w-[25vw]  '}>
+            <div className='flex flex-col border rounded p-20'>
+            <p className='text-3xl'>{info.name}</p>
+            <button   onClick={handleClick} className={theme === 'light' ? 'lightButton' : 'blackButton'}>подробнее</button> 
+            <input type="checkbox" className='mb-2' name={info.name} checked={checked} id={info.height}  value={info.name} onChange={(e)=> handleSelected(e)}></input>
+            </div>
+           
+           {useOpen && <DetailedCard setUseOpen={setUseOpen} name={info.name}   />}
+         </div>
+       
+        
+        )
+        
+   
+  
 }
 
 export default Card
+
