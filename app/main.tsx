5dc9fde3c93ff274a4c431cea5b37e06a5612db9
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-02-23 20:09:57 +0300

    Update main.tsx

--- app/main.tsx
+++ app/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './Pages/App.tsx'
+import App from './Pages/App'
 import { BrowserRouter, Routes, Route } from "react-router";
-import Problem from './Pages/Problem.tsx';
+import Problem from './Pages/Problem';
 import { Provider } from 'react-redux'
-import { store } from './store.ts';
+import { store } from './store';
 
 
 
