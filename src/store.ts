e98ec0e57ffd1e5b946b8258a0a2a5cffc1d1480
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-03-09 19:51:58 +0300

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -1,17 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { starWarsApi } from "./api/StarWarsAPI";
 import { createWrapper } from "next-redux-wrapper";
 
  import selectedReducer from './StoreSelected'
 
 export const store = () => configureStore({
      reducer: {
-        [starWarsApi.reducerPath]: starWarsApi.reducer,
+ 
         selected: selectedReducer,
         
      },
-     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(starWarsApi.middleware),
+
 
 })
 
