9ac3d644e286e1d5e6f0c9ca62e8f19e3ce42e02
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-03-09 19:20:59 +0300

    Update StoreSelected.tsx

--- app/StoreSelected.tsx
+++ app/StoreSelected.tsx
@@ -1,48 +1,57 @@
-import { createSlice} from "@reduxjs/toolkit"
-import type {  PayloadAction } from "@reduxjs/toolkit"
+import { createSlice, type PayloadAction  } from "@reduxjs/toolkit"
 
-const initState = [] as string[]
+interface DeatilesPerson {
+  birth_year?: string;
+  created?: string;
+  edited?: string;
+  eye_color?: string;
+  films?: string[];
+  gender?: string;
+  hair_color?: string;
+  height?: string;
+  homeworld?: string;
+  mass?: string;
+  name?: string;
+  skin_color?: string;
+  species?: string[];
+  starships?: string[];
+  url?: string;
+  vehicles?: string[];
+}
+
+interface State {
+  selectedPeople: DeatilesPerson[];
+}
+const initState = {
+  selectedPeople: [],
+}
 
 const StoreSelecedSlice = createSlice({
     name: 'selectedItems',
-    initialState: initState,
+    initialState: initState as State,
     reducers: {
-        selected: (state, action: PayloadAction<string>) => {
-           state.push(action.payload)
-      
+        selected: (state, action: PayloadAction<DeatilesPerson>) => {
+           state.selectedPeople.push(action.payload)
+           
         },
         removeItemSelected: (state, action)=> {
-             return  state.filter((state)=> state !==action.payload)
-                      
-             
-                //  return {
-                //   todos: state.todos.map(todo => {
-                //     if(todo.value !== action.payload){
-                //       return todo
-                //     }
-                //   return todo
-                  
-                  
-                  
-                //   })
-                //  }
-            
+             return {
+              selectedPeople:   [ ...state.selectedPeople].filter((x)=> x.name !==action.payload)
+             } 
         },
         removeAllSelected: ()=> {
-      return  []  
+      return {selectedPeople: []}
        
         
 
     },
     getCheckboxState: (state)=> {
                    return state
-    },
-   
+    },   
     }
-
 })
-
   export const {selected,  removeItemSelected,getCheckboxState, removeAllSelected } = StoreSelecedSlice.actions
 export default StoreSelecedSlice.reducer
+
 
 
