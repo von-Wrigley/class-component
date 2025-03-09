26f484b306fe660ef826ddf026ae926ba38d7aad
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-03-09 19:31:05 +0300

    Add files via upload

---
+++ src/Components/Pagination.tsx
@@ -0,0 +1,38 @@
+import { useRouter } from "next/router";
+
+
+function Pagination({totalPages}:{totalPages:number}) {
+const router = useRouter()
+
+const handlepagination = (page:number) => {
+  router.push(`?page=${page}`, undefined, { shallow: true });
+}
+
+
+return (
+    
+    <>
+  
+    <div >
+    {Array.from({ length: totalPages/4 }, (_, i) => i + 1).map((page) => (
+        <button
+          key={page}
+          onClick={() => handlepagination(page)}
+         className='bg-pink mr-2.5 text-white p-2 hover:cursor-pointer rounded'
+        >
+          {page}
+        </button>
+      ))}
+         
+     {/* {pages.map((page)=> 
+      (  <button key={page}  className='bg-black mr-2.5 text-white p-2 hover:cursor-pointer rounded' onClick={() => handlepagination(page)}>{page}</button>)
+     
+     )} */}
+     
+    </div>
+    </>
+    
+   
+  )
+}
+export default Pagination
