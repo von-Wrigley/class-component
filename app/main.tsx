e3d7c775b8750c975f5325df71c14b292a66f70b
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2025-03-09 19:22:08 +0300

    Update main.tsx

--- app/main.tsx
+++ app/main.tsx
@@ -1,22 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './Pages/App.tsx'
-import { BrowserRouter, Routes, Route } from "react-router";
-import Problem from './Pages/Problem.tsx';
-import { Provider } from 'react-redux'
-import { store } from './store.ts';
 import './index.css'
+// import Page from './pages/index.tsx'
+// import { BrowserRouter, Routes, Route } from "react-router";
+
+// import { Provider } from 'react-redux'
+// import { store } from './store.ts';
+
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}>
    <BrowserRouter>
     <Routes>
-      <Route index  element={<App />} />
-      <Route path="*" element={<Problem />} />
+      <Route index  element={<Page />} />
     </Routes>
     </BrowserRouter>
-    </Provider>
+    </Provider> */}
   </StrictMode>,
 )
+
+
